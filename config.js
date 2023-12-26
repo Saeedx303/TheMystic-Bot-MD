5ab4eacf397dc650c51c4ea94ee732d16726d4bb
Saeedx303 <130200805+Saeedx303@users.noreply.github.com>
2023-12-26 21:46:13 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-import {watchFile, unwatchFile} from 'fs';
+iimport {watchFile, unwatchFile} from 'fs';
 import chalk from 'chalk';
 import {fileURLToPath} from 'url';
 import fs from 'fs'; 
@@ -10,41 +10,28 @@ import moment from 'moment-timezone';
 global.botnumber = ""
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],
-  ['5212412377467', '💫 Mystic - Collaborator 6 💫', true],
-  ['5215517489568', '💫 Mystic - Rey Endymion 💫', false],
-  ['59895555511', '💫 Mystic - Collaborator 8 💫', true],
-  ['393518398856','💫 Mystic - Tester Fabri115💫', true],
-  ['593968585283'],
-  ['5219993404349'],
-  ['5219991402134'],
-  ['5492266466080'],
-  ['5219996125657'],
-  ['5218442114446'],
-  ['59894808483'],
-  ['593980586516'], 
-  ['595975740803'],  
-  ['5492266613038'],
-  ['50431953376'],
-  ['51906662557'],
-  ['595992611272']
+  ['923361501800', '👑 Mystic - Creador 👑', true],
+  ['923361501800', '💫 Mystic - Collaborator 1 💫', true],
+  ['923361501800', '💫 Mystic - Collaborator 2 💫', true],
+  ['923361501800', '💫 Mystic - Collaborator 3 💫', true],
+  ['923361501800', '💫 Mystic - Collaborator 4 💫', true],
+  ['923361501800', '💫 Mystic - Collaborator 5 💫', true],
+  ['923361501800', '💫 Mystic - Collaborator 6 💫', true],
+  ['923361501800', '💫 Mystic - Rey Endymion 💫', false],
+  ['923361501800', '💫 Mystic - Collaborator 8 💫', true],
+  ['923361501800','💫 Mystic - Tester Fabri115💫', true],
 ];
 
-global.suittag = ['5219993404349'];
-global.prems = ['51995386439'];
+global.suittag = ['923361501800'];
+global.prems = ['923361501800'];
 
 global.packname = 'Sticker';
-global.author = 'The Mystic - Bot';
-global.wm = 'The Mystic - Bot';
-global.titulowm = 'The Mystic - Bot';
-global.titulowm2 = `The Mystic - Bot`
-global.igfg = 'The Mystic - Bot';
-global.wait = '*[ ⏳ ] Cargando...*';
+global.author = 'The BABLI - Bot';
+global.wm = 'The BABLI - Bot';
+global.titulowm = 'The BABLI - Bot';
+global.titulowm2 = `The BABLI - Bot`
+global.igfg = 'The BABLI - Bot';
+global.wait = '*[ 💞 ] Cargando...*';
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
 global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
@@ -65,14 +52,14 @@ global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
 global.wm2 = `${dia} ${fecha}\nThe Mystic - Bot`;
-global.gt = 'The Mystic - Bot';
-global.mysticbot = 'The Mystic - Bot';
-global.md = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
-global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
-global.waitt = '*[ ⏳ ] Cargando...*';
-global.waittt = '*[ ⏳ ] Cargando...*';
-global.waitttt = '*[ ⏳ ] Cargando...*';
-global.nomorown = '5219993404349';
+global.gt = 'The BABLI - Bot';
+global.mysticbot = 'The BABLI - Bot';
+global.md = 'https://github.com/Saeedx303/TheMystic-Bot-MD';
+global.mysticbot = 'https://github.com/Saeedx303/TheMystic-Bot-MD';
+global.waitt = '*[ 💞 ] Cargando...*';
+global.waittt = '*[ 💞 ] Cargando...*';
+global.waitttt = '*[ 💞 ] Cargando...*';
+global.nomorown = '923361501800';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
@@ -87,8 +74,8 @@ global.htki = '*⭑•̩̩͙⊱•••• ☪*';
 global.htka = '*☪ ••••̩̩͙⊰•⭑*';
 global.comienzo = '• • ◕◕════';
 global.fin = '════◕◕ • •';
-global.botdate = `*[ 📅 ] Fecha:*  ${moment.tz('America/Mexico_City').format('DD/MM/YY')}`;
-global.bottime = `*[ ⏳ ] Hora:* ${moment.tz('America/Mexico_City').format('HH:mm:ss')}`;
+global.botdate = `*[ 🥿 ] Fecha:*  ${moment.tz('America/Mexico_City').format('DD/MM/YY')}`;
+global.bottime = `*[ 💞 ] Hora:* ${moment.tz('America/Mexico_City').format('HH:mm:ss')}`;
 global.fgif = {key: {participant: '0@s.whatsapp.net'}, message: {'videoMessage': {'title': wm, 'h': `Hmm`, 'seconds': '999999999', 'gifPlayback': 'true', 'caption': bottime, 'jpegThumbnail': fs.readFileSync('./Menu.png')}}};
 global.multiplier = 99;
 global.flaaa = [
